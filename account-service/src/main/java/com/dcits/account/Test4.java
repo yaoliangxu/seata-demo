0acc92c5f29566382039484a5019d7fbeb9f80a1
yaoliangxu <m15155428385@163.com>
2023-12-05 10:21:52 +0800

    修改11-新增

--- account-service/src/main/java/com/dcits/account/Test4.java
+++ account-service/src/main/java/com/dcits/account/Test4.java
@@ -4,5 +4,6 @@ public class Test4 {
     public static void main(String[] args) {
         System.out.println("测试8");
         System.out.println("测试10");
+        System.out.println("测试11");
     }
 }
