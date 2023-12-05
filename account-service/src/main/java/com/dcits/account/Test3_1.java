39ac26c50ad5bd9891affb2a9728d1deb3330234
yaoliangxu <m15155428385@163.com>
2023-12-05 09:49:03 +0800

    修改3

---
+++ account-service/src/main/java/com/dcits/account/Test3_1.java
@@ -0,0 +1,7 @@
+package com.dcits.account;
+
+public class Test3_1 {
+    public static void main(String[] args) {
+        System.out.println("12345678");
+    }
+}
---
+++ account-service/src/main/java/com/dcits/account/Test3_2.java
@@ -0,0 +1,7 @@
+package com.dcits.account;
+
+public class Test3_2 {
+    public static void main(String[] args) {
+        System.out.println("dfghjk");
+    }
+}
