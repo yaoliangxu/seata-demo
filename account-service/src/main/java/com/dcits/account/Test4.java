a1911512aae071263e74dcdb382bd1132154ca2c
yaoliangxu <m15155428385@163.com>
2023-12-05 14:36:06 +0800

    版本1

--- account-service/src/main/java/com/dcits/account/Test4.java
+++ account-service/src/main/java/com/dcits/account/Test4.java
@@ -2,13 +2,6 @@ package com.dcits.account;
 
 public class Test4 {
     public static void main(String[] args) {
-        System.out.println("测试8");
-        System.out.println("测试10");
-        System.out.println("测试12");
-        System.out.println("测试13");
-        System.out.println("默认");
-        System.out.println("版本回溯");
-        System.out.println("第二次版本回溯");
-        System.out.println("第三次版本回溯");
+        System.out.println("版本1");
     }
 }
