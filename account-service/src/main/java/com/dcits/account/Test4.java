3c3f0a3f5c24e04a7bc7256fc974aefbb8aa57f7
yaoliangxu <m15155428385@163.com>
2023-12-05 10:19:44 +0800

    修改10-修改

--- account-service/src/main/java/com/dcits/account/Test4.java
+++ account-service/src/main/java/com/dcits/account/Test4.java
@@ -3,6 +3,6 @@ package com.dcits.account;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("测试8");
-        System.out.println("测试9");
+        System.out.println("测试10");
     }
 }
